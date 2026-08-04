6a881eb46631a13dd450f15244e2a6c030c3713f
410-45-sairung <56905@yupparaj.ac.th>
2026-08-04 13:41:49 +0700

    Update vat_app.py

--- vat_app.py
+++ vat_app.py
@@ -1,8 +1,15 @@
+ import streamlit as st
 st.title("🛒แอปพลิเคชั่นคำนวณราคาสินค้ารวม VAT 7%")
-vat = price * 0.07
-st.header(f"• ภาษีมูลค่าเพิ่ม (VAT 7%): **{vat:.2f}** บาท")
-import streamlit as st
 price = st.number_input("กรอกราคาสินค้า (บาท):", value=0.0)
+vat = price * 0.07
 net_price = price - vat
-st.write("นางสาวสายรุ้ง ดิษยาวรโชติกุล เลขที่ 45  ม.4/10")
+
+
+st.header(f"• ภาษีมูลค่าเพิ่ม (VAT 7%): **{vat:.2f}** บาท")
+
+
 st.header(f"• ราคาสุทธิ: {net_price:.2f} บาท")
+
+
+st.divider()
+st.write("นางสาวสายรุ้ง ดิษยาวรโชจิกุล เลขที่ 45  ม.4/10")
