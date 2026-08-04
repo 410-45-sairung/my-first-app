2bfdd08a839db80a1a98a4574135ca482374867f
410-45-sairung <56905@yupparaj.ac.th>
2026-08-04 13:33:25 +0700

    vat_app.py

--- vat_app.py
+++ vat_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
 price = st.number_input("กรอกราคาสินค้า (บาท):", value=0.0)
-net_price = price - vat
+<net_price = price - vat.
 st.write("นางสาวสายรุ้ง ดิษยาวรโชติกุล เลขที่ 45  ม.4/10")
 st.header(f"• ราคาสุทธิ: {net_price:.2f} บาท")
